734cb60af1728eda0ea79fa1834e12ea24d50793
Jeff Oliver <jeffoliver3384@gmail.com>
2021-10-21 16:50:00 -0400

    First commmit, installed typescript dependencies, app cleanup

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className=''>
+      <Head>
+        <title>Instagram Clone</title>
+        {/* <link rel='icon' href='/favicon.ico' /> */}
+      </Head>
+
+      <h1>This is the Instagram build</h1>
+    </div>
+  );
+}
