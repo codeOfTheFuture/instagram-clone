c95e8807e32b95630ac4a22d9be92270599a440b
Jeff Oliver <jeffoliver3384@gmail.com>
2021-10-22 22:03:08 -0400

    Added styles to make navbar sticky and new messages count for messages icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import HeaderRight from "./HeaderRight";
 
 const Header: React.FC = () => {
   return (
-    <div>
+    <div className='shadow-sm border-b bg-white sticky top-0 z-50'>
       <div className='flex justify-between max-w-6xl mx-5 lg:mx-auto'>
         {/* Left */}
         <HeaderLeft />
