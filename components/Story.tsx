4316c5c25858716164dae7006f47f8538777b56a
Jeff Oliver <jeffoliver3384@gmail.com>
2021-10-22 23:30:15 -0400

    Stories and Story components complete

---
+++ components/Story.tsx
@@ -0,0 +1,19 @@
+interface StoryProps {
+  img: string;
+  username: string;
+}
+
+const Story: React.FC<StoryProps> = ({ img, username }) => {
+  return (
+    <div className=''>
+      <img
+        className='h-14 w-14 rounded-full p-[1.5px] border-red-500 border-2 object-contain cursor-pointer hover:scale-110 transition transform duration-200 ease-out'
+        src={img}
+        alt='story pic'
+      />
+      <p className='text-xs w-14 truncate text-center'>{username}</p>
+    </div>
+  );
+};
+
+export default Story;
