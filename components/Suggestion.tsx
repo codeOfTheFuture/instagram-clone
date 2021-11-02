b706be6cf288475c0552b257154e53d5077eb2e9
Jeff Oliver <jeffoliver3384@gmail.com>
2021-11-01 20:01:54 -0400

    WIP: Suggestions component

---
+++ components/Suggestion.tsx
@@ -0,0 +1,20 @@
+interface SuggestionProps {
+  avatar: string;
+  username: string;
+  companyName: string;
+}
+
+
+const Suggestion: React.FC<SuggestionProps> = ({ avatar, username, companyName }) => {
+  return (
+    <div className='flex items-center justify-between mt-3'>
+      <img className='w-10 h-10 rounded-full border p-[2px]' src={avatar} alt="avatar" />
+      <div className="flex-1 ml-4">
+        <h2 className="font-semibold text-sm">{username}</h2>
+        <h3 className='text-xs text-gray-400'>Works at {companyName}</h3>
+      </div>
+    </div>
+  )
+}
+
+export default Suggestion;
--- components/Suggestions.tsx
+++ components/Suggestions.tsx
@@ -1,7 +1,32 @@
+import { useState, useEffect } from "react";
+import faker from 'faker';
+import Suggestion from "./Suggestion";
+
 const Suggestions: React.FC = () => {
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const suggestions = [...Array(5)].map((_, i) => (
+      {
+        ...faker.helpers.contextualCard(),
+        id: i
+      }
+    ));
+    setSuggestions(suggestions);
+  }, [])
+
   return (
-    <div>
-      <h1>Suggestions</h1>
+    <div className='mt-4 ml-10'>
+      <div className="flex justify-between text-sm mb-5">
+        <h3 className='text-sm font-bold text-gray-400'>Suggestions for you</h3>
+        <button className='text-gray-600 font-semibold'>See all</button>
+      </div>
+      {
+        suggestions.map(profile => (
+          <Suggestion key={profile.id} avatar={profile.avatar} username={profile.username} companyName={profile.company.name} />
+
+        ))
+      }
     </div>
   )
 }
