d9a206c821b72252b007b8959343a3916549b927
Jeff Oliver <jeffoliver3384@gmail.com>
2021-11-24 03:59:50 -0500

    User can like a post

--- components/Post.tsx
+++ components/Post.tsx
@@ -7,11 +7,14 @@ import { FormEvent, useEffect, useState } from "react";
 import {
   addDoc,
   collection,
+  deleteDoc,
+  doc,
   DocumentData,
   onSnapshot,
   orderBy,
   query,
   serverTimestamp,
+  setDoc,
 } from "firebase/firestore";
 import { db } from "../firebase";
 import Comments from "./Comments";
@@ -25,7 +28,9 @@ const Post: React.FC<PostProps> = ({ post }) => {
     { username, profileImg, image, caption } = post.data(),
     { data: session } = useSession(),
     [comment, setComment] = useState<string>(""),
-    [comments, setComments] = useState<DocumentData[]>([]);
+    [comments, setComments] = useState<DocumentData[]>([]),
+    [likes, setLikes] = useState<DocumentData[]>([]),
+    [hasLiked, setHasLiked] = useState<boolean>(false);
 
   useEffect(
     () =>
@@ -36,8 +41,43 @@ const Post: React.FC<PostProps> = ({ post }) => {
         ),
         (snapshot: DocumentData) => setComments(snapshot.docs)
       ),
-    [db]
+    [db, id]
   );
+
+  useEffect(
+    () =>
+      onSnapshot(
+        collection(db, "posts", id, "likes"),
+        (snapshot: DocumentData) => setLikes(snapshot.docs)
+      ),
+    [db, id]
+  );
+
+  useEffect(
+    () =>
+      setHasLiked(
+        likes.findIndex((like) => like.id === session?.user?.uid) !== -1
+      ),
+    [likes]
+  );
+
+  const likePost = async () => {
+    if (hasLiked) {
+      try {
+        await deleteDoc(doc(db, "posts", id, "likes", session.user.uid));
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      try {
+        await setDoc(doc(db, "posts", id, "likes", session.user.uid), {
+          username: session.user.username,
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
 
   const handleCommentChange = (value: string) => {
     setComment(value);
@@ -66,7 +106,7 @@ const Post: React.FC<PostProps> = ({ post }) => {
 
       <img className='object-cover w-full' src={image} alt='Post Image' />
 
-      {session && <PostButtons />}
+      {session && <PostButtons likePost={likePost} hasLiked={hasLiked} />}
 
       <PostCaption username={username} caption={caption} />
 
--- components/PostButtons.tsx
+++ components/PostButtons.tsx
@@ -6,11 +6,23 @@ import {
 } from "@heroicons/react/outline";
 import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
 
-const PostButtons: React.FC = () => {
+interface PostButtonProps {
+  likePost: () => Promise<void>;
+  hasLiked: boolean;
+}
+
+const PostButtons: React.FC<PostButtonProps> = ({ likePost, hasLiked }) => {
   return (
     <div className='flex justify-between px-4 pt-4'>
       <div className='flex space-x-4'>
-        <HeartIcon className='postBtn' />
+        {hasLiked ? (
+          <HeartIconFilled
+            className='postBtn text-red-500'
+            onClick={likePost}
+          />
+        ) : (
+          <HeartIcon className='postBtn' onClick={likePost} />
+        )}
         <ChatIcon className='postBtn' />
         <PaperAirplaneIcon className='postBtn' />
       </div>
