f43b3f82dc17d659452395cf9cf72b5e257fbb37
Jeff Oliver <jeffoliver3384@gmail.com>
2021-11-24 01:49:13 -0500

    User can type comment in input box and comment is sent to firebase in sub collection for each post.

--- components/InputBox.tsx
+++ components/InputBox.tsx
@@ -1,17 +1,36 @@
-import { EmojiHappyIcon } from '@heroicons/react/outline'
+import { EmojiHappyIcon } from "@heroicons/react/outline";
+import { ChangeEvent, FormEvent } from "react";
 
-const InputBox: React.FC = () => {
+interface InputBoxProps {
+  comment: string;
+  handleCommentChange: (value: string) => void;
+  sendComment: (e: FormEvent<HTMLFormElement>) => Promise<void>;
+}
+
+const InputBox: React.FC<InputBoxProps> = (props) => {
+  const { comment, handleCommentChange, sendComment } = props;
+
   return (
-    <form className='flex items-center p-4'>
+    <form className='flex items-center p-4' onSubmit={sendComment}>
       <EmojiHappyIcon className='h-7' />
       <input
         className='border-none flex-1 focus:ring-0 outline-none'
         placeholder='Add a comment...'
-        type="text"
+        type='text'
+        value={comment}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          handleCommentChange(e.target.value)
+        }
       />
-      <button className='font-semibold text-blue-400'>Post</button>
+      <button
+        className='font-semibold text-blue-400'
+        type='submit'
+        disabled={!comment.trim()}
+      >
+        Post
+      </button>
     </form>
-  )
-}
+  );
+};
 
 export default InputBox;
--- components/Post.tsx
+++ components/Post.tsx
@@ -3,6 +3,17 @@ import PostButtons from "./PostButtons";
 import PostCaption from "./PostCaption";
 import PostHeader from "./PostHeader";
 import { useSession } from "next-auth/react";
+import { FormEvent, useEffect, useState } from "react";
+import {
+  addDoc,
+  collection,
+  DocumentData,
+  onSnapshot,
+  orderBy,
+  query,
+  serverTimestamp,
+} from "firebase/firestore";
+import { db } from "../firebase";
 
 interface PostProps {
   id: string;
@@ -14,15 +25,56 @@ interface PostProps {
 
 const Post: React.FC<PostProps> = (props) => {
   const { data: session } = useSession(),
-    { username, userImg, img, caption } = props;
+    { id, username, userImg, img, caption } = props,
+    [comment, setComment] = useState<string>(""),
+    [comments, setComments] = useState<DocumentData[]>([]);
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(
+          collection(db, "posts", id, "comments"),
+          orderBy("timestamp", "desc")
+        ),
+        (snapshot: DocumentData) => setComments(snapshot.docs)
+      ),
+    [db]
+  );
+
+  const handleCommentChange = (value: string) => {
+    setComment(value);
+  };
+
+  const sendComment = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const commentToSend = comment;
+    setComment("");
+
+    await addDoc(collection(db, "posts", id, "comments"), {
+      comment: commentToSend,
+      username: session.user.username,
+      userImg: session.user.image,
+      timestamp: serverTimestamp(),
+    });
+  };
 
   return (
     <div className='bg-white my-7 border rounded-sm'>
       <PostHeader username={username} userImg={userImg} />
+
       <img className='object-cover w-full' src={img} alt='Post Image' />
+
       {session && <PostButtons />}
+
       <PostCaption username={username} caption={caption} />
-      {session && <InputBox />}
+
+      {session && (
+        <InputBox
+          comment={comment}
+          handleCommentChange={handleCommentChange}
+          sendComment={sendComment}
+        />
+      )}
     </div>
   );
 };
