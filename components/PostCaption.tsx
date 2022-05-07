f8189d91835ef0394f5aa4e44c6f3f1b5669eda8
Jeff Oliver <jeffoliver3384@gmail.com>
2022-05-07 03:36:14 -0400

    fixed warning in post caption, change p tag nested in another p tag in post caption to a div tag

--- components/PostCaption.tsx
+++ components/PostCaption.tsx
@@ -8,11 +8,11 @@ const PostCaption: React.FC<PostCaptionProps> = (props) => {
   const { username, caption, likes } = props;
   return (
     <>
-      <p className='p-5 truncate'>
+      <div className='p-5 truncate'>
         {likes > 0 && <p className='font-bold mb-1'>{likes} likes</p>}
         <span className='font-bold mr-1'>{username} </span>
         {caption}
-      </p>
+      </div>
     </>
   );
 };
